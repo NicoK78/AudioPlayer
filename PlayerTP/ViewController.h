a230af18cce1ef1f0370013bd3e2a5d0fa60b344
LoisMenissier <lois@lois.lois>
2015-03-18 09:54:26 +0100

    Initial Commit

---
+++ PlayerTP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayerTP
+//
+//  Created by Loïs Ménissier on 18/03/2015.
+//  Copyright (c) 2015 Loïs Ménissier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PlayerTP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlayerTP
+//
+//  Created by Loïs Ménissier on 18/03/2015.
+//  Copyright (c) 2015 Loïs Ménissier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
